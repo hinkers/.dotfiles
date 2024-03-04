d3730358399382318952664b7c20417d5e9c3af7
hinkers <24424803+hinkers@users.noreply.github.com>
2024-03-04 13:17:42 +1100

    Autocomplete still doesn't work

--- .config/nvim/lua/hinkers/plugins/lsp.lua
+++ .config/nvim/lua/hinkers/plugins/lsp.lua
@@ -1,18 +1,101 @@
 return {
-    -- {
-    --     "nvim-treesitter/nvim-treesitter",
-    --     config = function()
-    --         require('nvim-treesitter.configs').setup({
-    --             prefer_git = false,
-    --             compilers = { "zig" },
-    --             ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query", "bash", "go" },
-    --             sync_install = false,
-    --             auto_install = true,
-    --             highlight = {
-    --                 enable = true,
-    --                 additional_vim_regex_highlighting = false,
-    --             },
-    --         })
-    --     end,
-    -- },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                prefer_git = false,
+                compilers = { "zig" },
+                ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query", "bash", "go" },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+        config = function()
+            local lspconfig = require('lspconfig')
+
+            -- Global mappings.
+            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+        end
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "saadparwaiz1/cmp_luasnip",
+            "L3MON4D3/LuaSnip",
+        },
+        config = function()
+            local luasnip = require('luasnip')
+            local cmp = require('cmp')
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+                    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<CR>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    },
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                }),
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                },
+            })
+        end
+    },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require('mason').setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup()
+        end
+    },
 }
--- scripts/update.bat
+++ scripts/update.bat
@@ -1,13 +1,4 @@
 @echo off
 
-rem Define the packer installation path
-set PACKER_INSTALL_PATH=%LOCALAPPDATA%\nvim\site\pack\packer\start\packer.nvim
-
-rem Check if packer is installed
-if not exist "%PACKER_INSTALL_PATH%" (
-    echo Packer not found. Installing packer.nvim...
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim "%PACKER_INSTALL_PATH%"
-)
-
 rem Copy nvim config
 xcopy /E /I config\nvim\* %LOCALAPPDATA%\nvim\
